73c150861564e6549de9e4b994149740089b2119
wostrau <a.ostrovtsov@gmail.com>
2023-09-06 15:54:03 +0200

    added logic for getting winner in the game

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const startGameBtn = document.getElementById('start-game-btn'); 
+const startGameBtn = document.getElementById('start-game-btn');
 
 const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 
-const getPlayerChoice = function() {
+let gameIsRunning = false;
+
+const getPlayerChoice = function () {
   const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
   if (
     selection !== ROCK &&
@@ -18,8 +23,37 @@ const getPlayerChoice = function() {
   return selection;
 };
 
-startGameBtn.addEventListener('click', function() {
+const getComputerChoice = () => {
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
+const getWinner = (cChoice, pChoice) => {
+  if (cChoice === pChoice) {
+    return RESULT_DRAW;
+  } else if (
+    (cChoice === ROCK && pChoice === PAPER) ||
+    (cChoice === PAPER && pChoice === SCISSORS) ||
+    (cChoice === SCISSORS && pChoice === ROCK)
+  ) {
+    return RESULT_PLAYER_WINS;
+  } else {
+    return RESULT_COMPUTER_WINS;
+  }
+};
+
+startGameBtn.addEventListener('click', function () {
+  if (gameIsRunning) return;
+  gameIsRunning = true;
   console.log('Game is starting...');
-  const playerSelection = getPlayerChoice();
-  console.log(playerSelection);
+  const playerChoice = getPlayerChoice();
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(playerChoice, computerChoice);
+  console.log(winner);
 });
